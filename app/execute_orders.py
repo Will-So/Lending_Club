1fb9e47ba93c6e15724be8f63942c3c9f4d44e8c
Will Sorenson <ryan.d.sorenson@gmail.com>
2015-11-17 14:32:56 -0500

    make logging work'

--- app/execute_orders.py
+++ app/execute_orders.py
@@ -19,7 +19,7 @@ import os
 import logging
 
 # Set Order Settings
-roi_floor = .10
+roi_floor = .12
 default_floor = .20
 amount = 25
 
@@ -36,9 +36,7 @@ portfolio_id = 65013027
 # Init Logging
 logging.basicConfig(filename='../orders.log',level=logging.DEBUG)
 logging.getLogger("requests").setLevel(logging.WARNING)
-
-# TODO: Verify that logging is working and then remove print statements
-# TODO: Verify that
+logging.getLogger('werkzeug').setLevel(logging.WARNING)
 
 
 def has_enough_cash():
@@ -66,19 +64,17 @@ def _main():
     for row in c.execute('SELECT * FROM orders'):
         already_ordered.append(row[1])
 
-    print("Placing orders for {} loans \n{} already ordered"
+    logging.debug("Placing orders for {} loans \n{} already ordered"
             .format(len(set((good_ids)) - set(already_ordered)), len(already_ordered)))
 
     for id in good_ids:
         if id not in already_ordered and has_enough_cash():
              place_order(id)
         elif not has_enough_cash():
-            print("No cash left")
             logging.debug("No Cash left")
             break
 
     logging.debug("Finished Ordering")
-    print("Finished ordering")
 
 
 def place_order(id):
@@ -86,10 +82,8 @@ def place_order(id):
     Places an order and appends the ordered loan
 
     """
-
-    print("Test Submitting Order for {}".format(id))
-    logging.debug("Submitting Order for {}".format(id))
     now = arrow.utcnow().format('YYYY-MM-DDTHH:mm:ss')
+    logging.debug("Submitting Order for {} at {}".format(id, now))
 
     payload = {"aid": '{}'.format(investor_id),
                "orders": [
@@ -100,22 +94,18 @@ def place_order(id):
                 }
                 ]}
 
-    print(payload)
     logging.debug("This is the payload sent: {}".format(payload))
 
     r = requests.post('https://api.lendingclub.com/api/investor/v1/accounts/{}/orders'
                       .format(investor_id), json=payload,
                       headers= headers)
 
-    print(r.status_code)
-    print(r.text)
-    
     if r.status_code == 200:
         c.execute("""INSERT INTO orders VALUES ("{0}", {1}, {2})"""
                     .format(now, id, amount))
         conn.commit()
         
-        logging.debug("Text for id {} : {} at {}".format(id, r.text, now))
+        logging.debug("SUCCESS: for id {} : {} at {} successful".format(id, r.text, now))
     else:
         logging.debug("Order for id {} failed. {} at {}".format(id, r.status_code, now))
 
