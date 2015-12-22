d17eb04faa9a8e4867882526210fbb2dc02e66b4
Will Sorenson <ryan.d.sorenson@gmail.com>
2015-12-23 06:44:31 +0700

    test infinite values

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,10 +1,29 @@
 """
 Tests whether the `process_api` module can handle weird values being fed to the dataframe
 """
+import py.test
+import engarde
+import numpy as np
 
-def test_inf_values(df):
-    pass
 
-def test_not_all_states(df):
-    pass
+from app.process_api import generate_completed_df
+
+test_df = generate_completed_df()
+
+def test_inf_values():
+    """
+    Ensures that all values are finite.
+
+    Notes
+    ---
+    When processing the data from the API, the values that could cause np.inf should be removed
+    automatically.
+    """
+    assert not any(np.isinf(test_df))
+
+
+def test_categories():
+    """
+    Ensures that all categories are added to API even if not present in the API Data.
+    """
 
