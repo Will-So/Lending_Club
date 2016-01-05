3dffd88a83a0220f3a6dc5b02b9985d5588da96c
Will Sorenson <ryan.d.sorenson@gmail.com>
2016-01-06 02:34:40 +0700

    Verify omitted categories work

--- tests/config_bad_data.py
+++ tests/config_bad_data.py
@@ -5,3 +5,12 @@ import pandas as pd
 
 df = pd.read_csv('good_data.csv')
 
+# Have some categories be missing.
+df = df[df.purpose != 'credit_card']
+df = df[df.home_ownership != 'RENT']
+df = df[df.grade != 'A']
+
+# Populate with 0 values
+df.annual_inc.iloc[[2,3,45,12]] = 0
+
+pd.to_csv('bad_data.csv')
