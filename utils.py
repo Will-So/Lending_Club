0d9a19c03f4e56af08bc3c8a4b026f190a3b80b7
Will Sorenson <ryan.d.sorenson@gmail.com>
2016-01-10 15:13:17 -0500

    cleanup utils

--- utils.py
+++ utils.py
@@ -1,9 +1,14 @@
 import requests
 
 
-def get_portfolio_id(investor_id, header):
+def get_portfolio_id(investor_id, headers):
     """
     Retrieves portfolio ids
+    :param investor_id: int specifying the id of investor
+    :param headers: header to be sent with the request. Usually just the authoriation
+    :return: JSON of the request.
     """
     r = requests.get('https://api.lendingclub.com/api/investor/v1/accounts/{}/portfolios'
                       .format(investor_id), headers=headers)
+
+    return r
