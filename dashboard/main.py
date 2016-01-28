97290582093e53d09069d4f0b62d7462d6f99adc
Will Sorenson <ryan.d.sorenson@gmail.com>
2016-01-28 13:52:25 -0500

    init on dashboard

--- dashboard/main.py
+++ dashboard/main.py
@@ -5,6 +5,9 @@ Steps:
 """
 from bokeh.plotting import figure, show, output_server
 
+from bokeh.models.widgets import Slider, Select, TextInput
+
+
 from app.process_api import generate_completed_df
 
 df = generate_completed_df()
@@ -12,8 +15,14 @@ df = generate_completed_df()
 p = figure(title="Predictions for Currently Available Notes")
 
 
-def generate_aggregated_roi():
+def generate_aggregated_roi(selected_notes):
     """
+    Generates the aggregated ROI of
 
     :return:
     """
+
+
+output_server("")
+
+axis_map = {'ROI': }
