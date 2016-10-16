c4c12a88df29f89b207ee727699dee7c41d9b931
Will-So <wil.sorenson@gmail.com>
2016-10-16 15:03:16 -0700

    add placeholder for new accounts

--- app/config.py
+++ app/config.py
@@ -11,3 +11,9 @@ main_config = dict(credentials=os.environ['LENDING_CLUB_API'], investor_id=58092
 
 iteration_2 = dict(credentials=os.environ['LENDING_CLUB_API'], investor_id=5809260,
                    portfolio_id=70552224) # Iteration 2 config
+
+will_ira = dict(credentials=os.environ['IRA_LENDING_CLUB_API'], investor_id=None,
+             portfolio_id = None)
+
+ira_2 = dict(credentials=os.environ['IRA_2_LENDING_CLUB_API'], investor_id=None,
+             portfolio_id = None)
